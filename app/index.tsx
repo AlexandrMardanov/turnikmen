ddc23394663dc90faa3a8ad6a19387fed6856949
Oleksandr Mardanov <oleksandr.mardanov@patrianna.com>
2026-01-17 22:02:19 +0200

    Enhance project configuration and styling
    - Added `babel.config.js` for Babel configuration with NativeWind support.
    - Introduced `global.css` for Tailwind CSS styles.
    - Created `metro.config.js` to integrate NativeWind with Metro bundler.
    - Updated `app.json` to include the Metro bundler configuration.
    - Added `tailwind.config.js` for Tailwind CSS setup.
    - Updated `package.json` with new dependencies for NativeWind and Tailwind CSS.
    - Included `nativewind-env.d.ts` for TypeScript support with NativeWind.
    - Modified `tsconfig.json` to reference the new TypeScript definition file.
    - Updated `_layout.tsx` to import global styles.

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "red",
-      }}
-    >
-      <Text style={{ color: "white" }}>
+    <View className="flex-1 justify-center items-center bg-red-500">
+      <Text className="text-xxl font-bold text-white">
         Edit app/index.tsx to edit this screen.
       </Text>
     </View>
