a590d35a11cea48f2d696647f77d9197bee388cb
Oleksandr Mardanov <oleksandr.mardanov@patrianna.com>
2026-01-18 03:57:24 +0200

    Update dependencies and enhance index screen
    - Added `react-native-screens` version 4.16.0 to improve navigation performance.
    - Updated `eslint`, `@typescript-eslint/eslint-plugin`, and `@typescript-eslint/parser` to version 8.53.0 for better linting support.
    - Upgraded `eslint` to version 9.39.2 for improved code quality checks.
    - Modified the text in `app/index.tsx` to provide clearer instructions for editing the screen.

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>
+        Edit Hello World app/index.tsx to edit this screen. !!! asd asdasdas asdasd asdasd asd ad asd
+      </Text>
     </View>
   );
 }
